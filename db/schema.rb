06f60a0684b3e82122b06642fe62a0ed1f4f2306
Vasco Ribeiro <vduarterib@icloud.com>
2016-05-16 18:46:47 +0200

    weapons views

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160516140810) do
+ActiveRecord::Schema.define(version: 20160516153439) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -34,14 +34,26 @@ ActiveRecord::Schema.define(version: 20160516140810) do
   add_index "bookings", ["weapon_id"], name: "index_bookings_on_weapon_id", using: :btree
 
   create_table "users", force: :cascade do |t|
+    t.string   "email"
+    t.string   "encrypted_password",     default: "", null: false
+    t.string   "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.integer  "sign_in_count",          default: 0,  null: false
+    t.datetime "current_sign_in_at"
+    t.datetime "last_sign_in_at"
+    t.inet     "current_sign_in_ip"
+    t.inet     "last_sign_in_ip"
     t.string   "name"
     t.text     "description"
     t.string   "avatar"
     t.string   "phone"
-    t.string   "email"
-    t.datetime "created_at",  null: false
-    t.datetime "updated_at",  null: false
+    t.datetime "created_at",                          null: false
+    t.datetime "updated_at",                          null: false
   end
+
+  add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
+  add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
 
   create_table "weapons", force: :cascade do |t|
     t.string   "name"
@@ -56,6 +68,7 @@ ActiveRecord::Schema.define(version: 20160516140810) do
     t.integer  "user_id"
     t.datetime "created_at",            null: false
     t.datetime "updated_at",            null: false
+    t.string   "category"
   end
 
   add_index "weapons", ["user_id"], name: "index_weapons_on_user_id", using: :btree
