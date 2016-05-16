3e0ab7354612def1e1d4b36f2f56add31fe71bd2
nicolas evin <nicolas.evin@gmail.com>
2016-05-16 18:07:58 +0200

    added bookings controller

---
+++ app/controllers/bookings_controller.rb
@@ -0,0 +1,18 @@
+class BookingsController < ApplicationController
+  def show
+    @booking = Booking.find(params[:id])
+  end
+
+  def create
+    @booking = Booking.find(params[:id])
+    @booking = Booking.new(booking_params)
+    @booking.save
+    redirect_to booking_path(@booking)
+  end
+
+  private
+
+  def booking_params
+  params.require(@booking).permit(:weapon, :user, :start_date_on, :end_date_on, :price_paid)
+  end
+end
--- app/controllers/weapons_controller.rb
+++ app/controllers/weapons_controller.rb
@@ -6,6 +6,7 @@ class WeaponsController < ApplicationController
   end
 
   def show
+    @booking = Booking.new
   end
 
   def new
