a078a4a51969c2b3f229bb28b0742dc2c6b56c31
Vasco Ribeiro <vduarterib@icloud.com>
2016-05-17 13:27:29 +0200

    add view index

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,11 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
+
   resources :weapons do
     resources :bookings, only: [:create, :show]
   end
+end
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
@@ -58,4 +60,4 @@ Rails.application.routes.draw do
   #     # (app/controllers/admin/products_controller.rb)
   #     resources :products
   #   end
-end
+
