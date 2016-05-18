e2fd3f7b4277979318cb6aa64b6b0ed50ccbfa18
hopolivier <open.lelong@laposte.net>
2016-05-18 19:22:34 +0200

    added dynamic _card and fixed dbseed

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,8 +8,8 @@ require 'faker'
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
-User.destroy_all
 Weapon.destroy_all
+User.destroy_all
 
 # 6.times do
 #   Weapon.create(name: "Self defense rifle", category: "Assault riffle", description:"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.", address: "Paris", price: "120")
@@ -22,22 +22,22 @@ Weapon.destroy_all
 # b.user = a
 # b.save
 
-a = User.create(name: "Priscilla", description: "I love big guns", picture: File.open(Rails.root.join('db/seed/user_2.jpg')))
+a = User.create(name: "Priscilla", description: "I love big guns", email: "Priscilla@gmail.com", password: "ioueroui", picture: File.open(Rails.root.join('db/seed/user_2.jpg')))
 b = Weapon.new(name: "Self defense rifle", category: "Assault riffle", picture: File.open(Rails.root.join('db/seed/weapon_2.jpg')), description: Faker::Lorem.paragraph(2), address: Faker::Address.street_address + ' ' + Faker::Address.zip_code + ' ' + Faker::Address.city, price: rand(200..5000))
 b.user = a
 b.save
 
-c = User.create(name: "Grandma Nova", description: "Once upon a time..." , picture: File.open(Rails.root.join('db/seed/user_3.jpg')))
+c = User.create(name: "Grandma Nova", description: "Once upon a time...", email: "Grandma@gmail.com", password: "ioueroui", picture: File.open(Rails.root.join('db/seed/user_3.jpg')))
 d = Weapon.new(name: "WWII rifle", category: "Assault riffle", picture: File.open(Rails.root.join('db/seed/weapon_4.jpg')), description: Faker::Lorem.paragraph(2), address: Faker::Address.street_address + ' ' + Faker::Address.zip_code + ' ' + Faker::Address.city, price: rand(200..5000))
 d.user = c
 d.save
 
-e = User.create(name: "Ryan", description: "Once upon a time..." , picture: File.open(Rails.root.join('db/seed/user_4.jpg')))
+e = User.create(name: "Ryan", description: "Once upon a time...", email: "Ryan@gmail.com", password: "ioueroui", picture: File.open(Rails.root.join('db/seed/user_4.jpg')))
 f = Weapon.new(name: "Militart tank", category: "Heavy weapon", picture: File.open(Rails.root.join('db/seed/weapon_4.jpg')), description: Faker::Lorem.paragraph(2), address: Faker::Address.street_address + ' ' + Faker::Address.zip_code + ' ' + Faker::Address.city, price: rand(200..5000))
 f.user = e
 f.save
 
-g = User.create(name: "Donald", description: "NRA supporter" , picture: File.open(Rails.root.join('db/seed/user_6.jpg')))
+g = User.create(name: "Donald", description: "NRA supporter", email: "Donald@gmail.com", password: "ioueroui", picture: File.open(Rails.root.join('db/seed/user_6.jpg')))
 h = Weapon.new(name: "Feel my gun power", category: "Assault riffle", picture: File.open(Rails.root.join('db/seed/weapon_6.jpg')), description: Faker::Lorem.paragraph(2), address: Faker::Address.street_address + ' ' + Faker::Address.zip_code + ' ' + Faker::Address.city, price: rand(200..5000))
 h.user = g
 h.save
