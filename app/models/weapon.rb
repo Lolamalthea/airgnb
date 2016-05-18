50e4c41fb9c5e94321b146830a85ed795745363f
nicolas evin <nicolas.evin@gmail.com>
2016-05-18 10:09:25 +0200

    updated weapon categories

--- app/models/weapon.rb
+++ app/models/weapon.rb
@@ -2,7 +2,7 @@ class Weapon < ActiveRecord::Base
   belongs_to :user
   has_many :bookings
 
-CATEGORIES = ["Kalach", "gun"]
+CATEGORIES = ["Revolver", "Riffle", "Kalachnikov", "Grenade launcher", "Other"]
 
   validates :name, presence: true
   # validates :category, inclusion: { in: CATEGORIES, allow_nil: false }
