c2d55139d14083ddd3458569a93e5e92b85e6ac4
hopolivier <open.lelong@laposte.net>
2016-05-19 17:19:42 +0200

    modified users routing

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -28,7 +28,7 @@ class UsersController < ApplicationController
   end
 
   def set_user
-    @user = user.find(params[:id])
+    @user = User.find(params[:id])
   end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
+  # devise_for :users, controllers: { omniauth_callbacks: 'users/omniauth_callbacks', registrations: 'registrations' }
   devise_for :users, controllers: { omniauth_callbacks: 'users/omniauth_callbacks', registrations: 'registrations' }
+
+  resources :users, only: [:edit, :update]
 
   root to: 'pages#home'
 
